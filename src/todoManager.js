4eafcf9926b44a4cc760815454ff46cdedf763c0
basurakid <92126346+basurakid@users.noreply.github.com>
2025-03-18 13:33:19 +0100

    Create sidebar with styling

---
+++ src/todoManager.js
@@ -0,0 +1,17 @@
+
+const projects = [];
+
+class ToDo {
+    constructor(title, description, dueDate, priority){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}
+
+class Project {
+    constructor(title){
+        this.title = title;
+    }
+}
